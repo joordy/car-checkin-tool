86b907131656c78b8bbddd9f6e502a8461d75066
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-05-24 17:47:52 +0200

    Inline button

---
+++ src/components/atoms/InlineButton/InlineButton.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Button = styled.p`
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+        'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 1em;
+    color: #666782;
+    a {
+        font-weight: 700;
+        color: #1268e8;
+        text-decoration: none;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+`
