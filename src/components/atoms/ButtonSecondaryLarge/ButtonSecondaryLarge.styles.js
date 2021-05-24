bd8be6a37f25a94ffd4a7f0d5c82bcd75f212403
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-05-24 17:47:18 +0200

    Buttons secondary

---
+++ src/components/atoms/ButtonSecondaryLarge/ButtonSecondaryLarge.styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Button = styled.a`
+    width: 284px;
+    max-height: 48px;
+    border-radius: 8px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+        'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 700;
+    font-size: 1em;
+    text-align: center;
+    padding: 1.5em 0;
+    background-color: #ffffff;
+    border: 0.15em solid #009900;
+    color: #009900;
+    text-decoration: none;
+    &:hover {
+        cursor: pointer;
+        background-color: #009900;
+        color: #ffffff;
+    }
+`
