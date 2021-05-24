596f974d6c3019e070b45938a1ff3c311ca5ef7f
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-05-24 16:14:43 +0200

    Add Icons

---
+++ src/components/atoms/CameraIcon/CameraIcon.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Icon = styled.span`
+    width: ${(props) => props.width || '100%'};
+    height: ${(props) => props.height || '100%'};
+    svg {
+        width: 100%;
+        height: 100%;
+        opacity: ${(props) => props.opacity || '1'};
+        path {
+            stroke: ${(props) => props.fill || '#ffffff'};
+        }
+    }
+`
---
+++ src/components/atoms/EyeOffIcon/EyeOffIcon.jsx
@@ -0,0 +1,33 @@
+// Imports
+import * as Styles from './EyeOffIcon.styles.js'
+
+// Component
+const EyeOffIcon = ({ width, height, opacity, fill }) => {
+    return (
+        <Styles.Icon width={width} height={height} opacity={opacity} fill={fill}>
+            <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path
+                    d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path d="M1 1L23 23" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+        </Styles.Icon>
+    )
+}
+
+export default EyeOffIcon
