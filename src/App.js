839a4a3e39f48fe6c77aad0d8e4a600702596984
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-05-25 16:30:24 +0200

    Add route /checkin

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import GlobalStyle from './styles/global'
 
 import { Header } from 'components/organisms/index'
-import { PageOne, PageTwo, Reservations } from 'components/templates/index'
+import { PageOne, PageTwo, Reservations, Checkin } from 'components/templates/index'
 
 function App() {
     console.log(`ENV: ${process.env.REACT_APP_TEST}`)
@@ -14,6 +14,7 @@ function App() {
                 <Route path="/" exact component={PageOne} />
                 <Route path="/Two" exact component={PageTwo} />
                 <Route path="/reservations" exact component={Reservations} />
+                <Route path="/checkin" exact component={Checkin} />
             </Switch>
             <GlobalStyle />
         </>
