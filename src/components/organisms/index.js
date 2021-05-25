9081f23bb8c737d1654f32caf0e83093d0b6f03b
joordy <jmfronik@gmail.com>
2021-05-25 16:39:53 +0200

    Implemented checkin logic

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -2,3 +2,4 @@
 export { default as Header } from './Header/Header'
 export { default as ReservationCard } from './Reservations/ReservationCard/ReservationCard'
 export { default as ReservationHeader } from './Reservations/ReservationHeader/ReservationHeader'
+export { default as StepsExplainer } from './Verification/StepsExplainer/StepsExplainer'
