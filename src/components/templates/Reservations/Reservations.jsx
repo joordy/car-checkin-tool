e9ff06b7c345ef36a167cb6d232f92023f1f4aa5
joordy <jmfronik@gmail.com>
2021-05-25 11:56:52 +0200

    Implemented Supabase, added two users and created dummy object for future

--- src/components/templates/Reservations/Reservations.jsx
+++ src/components/templates/Reservations/Reservations.jsx
@@ -1,9 +1,18 @@
 // React imports
 import React from 'react'
+import supabase from 'db/supabase.js'
 import * as Styles from './Reservations.styles.js'
 import { ReservationCard, ReservationHeader } from 'components/organisms/index'
+
 // React component
 const Reservations = () => {
+    const readDB = async () => {
+        const { data, error } = await supabase.from('users').select()
+        console.log(data)
+    }
+
+    readDB()
+
     return (
         <>
             <ReservationHeader />
---
+++ src/db/datamodel.js
@@ -0,0 +1,64 @@
+import { v4 as uuidv4 } from 'uuid'
+
+// Temporary user object
+const user = {
+    userID: uuidv4(),
+    firstName: 'Kelly',
+    lastName: 'de Jong',
+    email: 'kellydejong@gmail.com',
+    phone: '+31 6 1234 5678',
+    birthDate: '01-02-1997',
+    reservations: [
+        {
+            reservation: 202100002,
+            carImage:
+                'https://user-images.githubusercontent.com/48051912/119339694-c8330a00-bc91-11eb-9af5-3cde4338756e.png',
+            checkedIn: false,
+            pickUpDate: '28-05-2021',
+            pickUpTime: '10:30',
+            handInDate: '30-05-2021',
+            handInTime: '17:00',
+            class: 'A',
+            rentPrice: 499.99,
+            extraDriver: 1,
+            lowerOwnRisk: false,
+            otherInfo: {
+                ownRisk: 400,
+                deposit: 500,
+                freeKM: 600,
+                priceExtraKM: 0.3,
+            },
+            paidDeposit: false,
+            qrCode: '',
+        },
+        {
+            reservation: 202100003,
+            carImage:
+                'https://user-images.githubusercontent.com/48051912/119339694-c8330a00-bc91-11eb-9af5-3cde4338756e.png',
+            checkedIn: false,
+            pickUpDate: '20-05-2021',
+            pickUpTime: '10:30',
+            handInDate: '22-05-2021',
+            handInTime: '17:00',
+            class: 'Electric',
+            rentPrice: 699.99,
+            extraDriver: 1,
+            lowerOwnRisk: false,
+            otherInfo: {
+                ownRisk: 600,
+                deposit: 800,
+                freeKM: 600,
+                priceExtraKM: 0.3,
+            },
+            paidDeposit: false,
+            qrCode: '',
+        },
+    ],
+}
+
+// const insertDB = async () => {
+//     const { data, error } = await supabase.from('users').insert([user])
+//     console.log(data)
+//     console.log(error)
+// }
+// insertDB()
