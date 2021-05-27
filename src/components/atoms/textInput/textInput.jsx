1c1dbc2dba7955d62e44a791b9c309819d24298b
joordy <jmfronik@gmail.com>
2021-05-27 15:21:29 +0200

    Added folder for backend

--- src/components/atoms/textInput/textInput.jsx
+++
@@ -1,9 +0,0 @@
-// Imports
-import * as Styles from './textInput.styles.js'
-
-// Component
-const Input = ({ type, id, name, placeholder }) => {
-    return <Styles.Input type={type} id={id} name={name} placeholder={placeholder} />
-}
-
-export default Input
