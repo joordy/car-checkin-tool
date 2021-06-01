172b818fc0ec9b791baa424730c97023aafd7995
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-06-01 11:56:11 +0200

    Add new "single button wrapper"

--- client/src/components/molecules/index.js
+++ client/src/components/molecules/index.js
@@ -4,3 +4,4 @@ export { default as CheckinButtons } from './reservations/checkinButtons/checkin
 export { default as IconListItem } from './iconListItem/iconListItem.jsx'
 export { default as VerificationButtons } from './verificationButtons/verificationButtons.jsx'
 export { default as RadioInput } from './radioInput/radioInput.jsx'
+export { default as SingleButtonWrapper } from './singleButtonWrapper/singleButtonWrapper.jsx'
