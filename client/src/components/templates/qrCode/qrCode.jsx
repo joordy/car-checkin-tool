33a6b1e1347c82cad08f6f1cf3a61c8ea4cd6409
joordy <jmfronik@gmail.com>
2021-06-07 12:07:52 +0200

    add data to every route

--- client/src/components/templates/qrCode/qrCode.jsx
+++ client/src/components/templates/qrCode/qrCode.jsx
@@ -7,6 +7,19 @@ import { StepsExplainer, ShowQRCode } from 'components/organisms/index'
 // React component
 const QRCode = () => {
     const [reservations, setReservations] = useState([])
+    const [carReservation, setCarReservation] = useState([])
+
+    const getData = async () => {
+        const data = await fetch(`${process.env.REACT_APP_BACKEND}/order-details`)
+        console.log(data)
+        const response = await data.json()
+        if (response === 'undefined') {
+            console.log(response)
+            // window.location.href = '/reservations'
+        } else {
+            setCarReservation(response)
+        }
+    }
 
     const readDB = async () => {
         const { data, error } = await supabase.from('users').select()
@@ -14,8 +27,11 @@ const QRCode = () => {
     }
 
     useEffect(() => {
+        getData()
         readDB()
     }, [])
+
+    console.log('current reservation:', carReservation)
 
     let viewportHeight = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${viewportHeight}px`)
