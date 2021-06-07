34262835bb57a218a718ec305866485d34acec99
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-06-07 16:35:34 +0200

    updates

--- client/src/components/templates/orderDetails/orderDetails.jsx
+++ client/src/components/templates/orderDetails/orderDetails.jsx
@@ -42,9 +42,9 @@ const OrderDetails = () => {
     return (
         <Styles.Main className="page">
             <div className="stepsWrapper">
-                <StepsExplainer backLink="/reservations" step="0" reservation={currentCar} />
-                <CheckUserInfo reservation={currentCar} />
-                <CheckBookingInfo reservation={currentCar} />
+                <StepsExplainer backLink="/reservations" step="0" completedSteps={completedSteps} />
+                <CheckUserInfo />
+                <CheckBookingInfo />
             </div>
         </Styles.Main>
     )
