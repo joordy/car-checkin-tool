5bced566d584d08230518f2caf2fcb8ea320d6dc
tsjuusmei <guusmay2@hotmail.com>
2021-06-07 17:13:24 +0200

    using database information

--- server/routes/handler.js
+++ server/routes/handler.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fetch = require('node-fetch');
-const sgMail = require('@sendgrid/mail');
+const fetch = require("node-fetch");
+const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // const stripe = require('stripe')(`${process.env.STRIPE_TEST_KEY}`);
-const stripe = require('stripe')(
-  'sk_test_51IsTukJEAzd2OWuLk3FnSrJQnDxX3VuWZRtUIkCCvEBhK20GOantGHhar8kn1eqtYLtZ1qSX0hvVZ2lwyRWkCl5n002JbZmNr2'
+const stripe = require("stripe")(
+  "sk_test_51IsTukJEAzd2OWuLk3FnSrJQnDxX3VuWZRtUIkCCvEBhK20GOantGHhar8kn1eqtYLtZ1qSX0hvVZ2lwyRWkCl5n002JbZmNr2"
 );
 
 let reservation;
 
-router.get('/reservations', (req, res) => {
-  console.log('reservation page');
-  const str = 'reservation page';
+router.get("/reservations", (req, res) => {
+  console.log("reservation page");
+  const str = "reservation page";
   res.end(JSON.stringify(str));
 });
 
-router.post('/order-details', (req, res) => {
-  console.log('req.body', req.body);
+router.post("/order-details", (req, res) => {
+  // console.log("req.body", req.body);
   reservation = req.body;
   res.end(JSON.stringify(req.body));
 });
 
-router.get('/order-details', (req, res) => {
-  console.log('test, server approached');
+router.get("/order-details", (req, res) => {
+  console.log("test, server approached");
   console.log(reservation);
   const data = () => {
     if (!reservation) {
-      return 'undefined';
+      return "undefined";
     } else {
       return reservation;
     }
@@ -36,11 +36,11 @@ router.get('/order-details', (req, res) => {
   res.end(JSON.stringify(data()));
 });
 
-router.get('/verification', (req, res) => {
+router.get("/verification", (req, res) => {
   console.log(reservation);
   const data = () => {
     if (!reservation) {
-      return 'undefined';
+      return "undefined";
     } else {
       return reservation;
     }
@@ -49,11 +49,11 @@ router.get('/verification', (req, res) => {
   res.end(JSON.stringify(data()));
 });
 
-router.get('/deposit', (req, res) => {
+router.get("/deposit", (req, res) => {
   console.log(reservation);
   const data = () => {
     if (!reservation) {
-      return 'undefined';
+      return "undefined";
     } else {
       return reservation;
     }
@@ -62,17 +62,18 @@ router.get('/deposit', (req, res) => {
   res.end(JSON.stringify(data()));
 });
 
-router.post('/create-checkin', (req, res) => {
-  const { firstName, email, pickUpLocation, pickUpDateTime, reservationID } = req.body;
+router.post("/create-checkin", (req, res) => {
+  const { firstName, email, pickUpLocation, pickUpDateTime, reservationID } =
+    req.body;
 
   async function postData(url, data) {
     const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'same-origin',
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
       headers: {
         Authorization: `Basic ${process.env.WALLET_SECRET}`,
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
@@ -84,8 +85,8 @@ router.post('/create-checkin', (req, res) => {
       console.log(getDate(pickUpDateTime));
       const msg = {
         to: email,
-        from: { email: 'europauto2021@outlook.com', name: 'Europauto Checkin' },
-        templateId: 'd-d13520409a12422783f1f2bf35983b45',
+        from: { email: "europauto2021@outlook.com", name: "Europauto Checkin" },
+        templateId: "d-d13520409a12422783f1f2bf35983b45",
         dynamicTemplateData: {
           firstName: firstName,
           pickUpLocation: pickUpLocation,
@@ -97,36 +98,38 @@ router.post('/create-checkin', (req, res) => {
       sgMail
         .send(msg)
         .then(() => {
-          console.log('Email sent');
+          console.log("Email sent");
         })
         .catch((error) => {
           console.error(error);
         });
 
       res.send({
-        status: '200',
+        status: "200",
         serialNumber: data.serialNumber,
       });
     } else {
       res.send({
-        status: '404',
+        status: "404",
         errors: data.errors,
       });
     }
   });
 });
 
-router.post('/create-verification-session', async (req, res) => {
-  const verificationSession = await stripe.identity.verificationSessions.create({
-    type: 'document',
-    metadata: {
-      user_id: '{{USER_ID}}',
-    },
-  });
+router.post("/create-verification-session", async (req, res) => {
+  const verificationSession = await stripe.identity.verificationSessions.create(
+    {
+      type: "document",
+      metadata: {
+        user_id: "{{USER_ID}}",
+      },
+    }
+  );
 
   const clientSecret = verificationSession.client_secret;
 
-  console.log('User verification');
+  console.log("User verification");
   res.end(JSON.stringify(clientSecret));
 });
 
@@ -138,13 +141,13 @@ const calculateOrderAmount = (items) => {
   return 1400;
 };
 
-router.post('/create-payment-intent', async (req, res) => {
+router.post("/create-payment-intent", async (req, res) => {
   console.log(req.body);
   const { items } = req.body;
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: 'usd',
+    currency: "usd",
   });
   res.send({
     clientSecret: paymentIntent.client_secret,
@@ -152,14 +155,18 @@ router.post('/create-payment-intent', async (req, res) => {
 });
 
 function getDate(date) {
-  const dateTime = date.split(' ');
-  const dateElements = dateTime[0].split('-');
+  const dateTime = date.split(" ");
+  const dateElements = dateTime[0].split("-");
   const newDate = `${dateElements[2]}-${dateElements[1]}-${dateElements[0]} ${dateTime[1]}`;
   const dateObject = new Date(newDate);
-  const day = dateObject.toLocaleString('nl-NL', { day: 'numeric' });
-  const month = dateObject.toLocaleString('nl-NL', { month: 'long' });
-  const year = dateObject.toLocaleString('nl-NL', { year: 'numeric' });
-  const time = dateObject.toLocaleString('nl-NL', { hour: '2-digit', minute: '2-digit', hour12: false });
+  const day = dateObject.toLocaleString("nl-NL", { day: "numeric" });
+  const month = dateObject.toLocaleString("nl-NL", { month: "long" });
+  const year = dateObject.toLocaleString("nl-NL", { year: "numeric" });
+  const time = dateObject.toLocaleString("nl-NL", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
 
   return `${day} ${month} ${year} om ${time} uur`;
 }
