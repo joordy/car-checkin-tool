4df9a04d5d708284ac13cbca538aa56690ec767d
tsjuusmei <guusmay2@hotmail.com>
2021-06-14 15:53:54 +0200

    fix for responsive error message

--- src/components/molecules/login/loginError/loginError.styles.js
+++ src/components/molecules/login/loginError/loginError.styles.js
@@ -1,13 +1,17 @@
 import styled from 'styled-components'
+import { device } from 'styles/devices'
 
 export const MsgWrapper = styled.div`
     background-color: rgba(255, 0, 0, 0.15);
     border-radius: 5px;
     padding: 0 1em;
-    margin-top: -3.5em;
-    position: absolute;
     display: flex;
     svg {
         padding: 1em;
+    }
+
+    @media ${device.tablet} {
+        margin-top: -3.5em;
+        position: absolute;
     }
 `
