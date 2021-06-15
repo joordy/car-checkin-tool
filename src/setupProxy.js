db2b8f037eac875a3c1c2c80d619edd1d42e7b5a
joordy <jmfronik@gmail.com>
2021-06-15 20:15:48 +0200

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,63 @@ module.exports = function (app) {
         '/api',
         createProxyMiddleware({
             target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
-            // target: 'http://localhost:5001/car-check-in/us-central1/app',
+            //             // target: 'http://localhost:5001/car-check-in/us-central1/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/reservations',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/order-details',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/carIndexKey',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/testEnv',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/payMethod',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/create-verification-session',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/create-checkin',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
+            changeOrigin: true,
+        }),
+    )
+    app.use(
+        '/api/create-payment-intent',
+        createProxyMiddleware({
+            target: 'https://us-central1-car-check-in.cloudfunctions.net/app',
             changeOrigin: true,
         }),
     )
