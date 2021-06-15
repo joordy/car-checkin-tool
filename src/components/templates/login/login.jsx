f5e69b443fc91946f9a11a85f4ef1a41b0643f16
joordy <jmfronik@gmail.com>
2021-06-15 21:33:44 +0200

    fix

--- src/components/templates/login/login.jsx
+++ src/components/templates/login/login.jsx
@@ -18,6 +18,10 @@ const LoginPage = () => {
             const data = await res.json()
             console.log(data)
 
+            const res1 = await fetch('/users')
+            const data1 = await res1.json()
+            console.log(data1)
+
             // const res1 = await fetch('/users').then(async (res) => {
             //     const test = await res.clone().json()
             //     console.log(test)
